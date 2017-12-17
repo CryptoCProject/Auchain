7e12183be28d1746a738c9b647e158e717e86651
Zafeirium <34376272+Zafeirium@users.noreply.github.com>
2017-12-17 10:10:40 +0200

    Update Tools.java

--- src/auctioneum/utils/general/Tools.java
+++ src/auctioneum/utils/general/Tools.java
@@ -1,1 +1,54 @@
+package general;
+
+import java.util.HashMap;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class T {
+    
+    public static String SUCCESS = "1";
+    public static String NOT_SUCCESS = "2";
+    public static String NAME_EXIST = "3";
+    public static String WRONG_CREDENTIALS = "4";
+    public static String WRONG_OTP = "5";
+    public static String OTP_ERROR = "6";
+    public static String PRIVATE_KEY_ACK = "7";
+
+    public static String SIGN_UP = " a";
+    public static String SIGN_UP_CONFIRM = " b";
+
+    public static String LOG_IN = " c";
+    public static String LOG_IN_CONFIRM = " d";
+
+    public static String OTP = " e";
+    public static String OTP_CONFIRM = " f";
+
+    public static String MAIN = " g";
+    public static String MAIN_CONFIRM = " h";
+    
+    public static String PRIVATE_KEY = " i";
+    
+    public static JSONObject getJson (String ... strings) {
+        JSONObject jo = new JSONObject();
+        HashMap <String, String> map = new HashMap<>();
+        int i = 0;
+        try {
+            for(;;) {
+                try {
+                    map.put(strings[i], strings[i + 1]);
+                    i += 2;
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    break;
+                }
+            }
+            for (String k : map.keySet()) {
+                jo.put(k, map.get(k));
+            }
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return jo;
+    }
+    
+}
 
