49f09f2b86e7da567ed195088a1a6072a3c44bb6
Zafeirium <34376272+Zafeirium@users.noreply.github.com>
2017-12-17 11:40:01 +0200

    Add files via upload

---
+++ app/java/com/sec/secureapp/client/Client.java
@@ -0,0 +1,172 @@
+package com.sec.secureapp.client;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import com.sec.secureapp.general.T;
+
+public class Client extends Thread {
+
+    private boolean clientRunning;
+    private Context context;
+    private Socket connection = null; // socket to make the connection between Client-Server
+    private ObjectInputStream in = null; //stream for message that come up
+    private ObjectOutputStream out = null; //stream for messages that go to client
+    private myHandler handler; //handler is used to handle the messages are coming
+
+
+    public Client(Context context) {
+        this.clientRunning = true;
+        this.context = context;
+        handler = new myHandler();
+        handler.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            connectToServer();
+            setupStreams();
+            whileChatting();
+        } catch (EOFException eofe) {
+            System.out.println("TERMINATED!!!!!");
+            eofe.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("2 TERMINATED!!!!!");
+            ioe.printStackTrace();
+        } finally {
+            closeCrap();
+        }
+    }
+
+    public void connectToServer() throws IOException {
+        System.out.println("Attempting Connection");
+
+        try {
+            connection = new Socket(InetAddress.getByName(T.SERVER_IP), T.SERVER_PORT);
+            System.out.println("You are conected");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void setupStreams() throws IOException {
+        out = new ObjectOutputStream(connection.getOutputStream());
+        in = new ObjectInputStream(connection.getInputStream());
+        System.out.println("Client: Streams are now good to go");
+    }
+
+    public void whileChatting() throws IOException {
+        Object message;
+        while (this.clientRunning) {
+            try {
+                message = in.readObject();
+                Message message1 = new Message();
+                message1.obj = message;
+                handler.getHandler().sendMessage(message1);
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+                System.out.println("Class not found exception");
+            }
+        }
+    }
+
+    public void closeCrap() {
+        this.clientRunning = false;
+        System.out.println("Closing crap down");
+        try {
+            if (out != null) {
+                System.out.println("close out");
+                out.close();
+            }
+            if (in != null) {
+                System.out.println("close in");
+                in.close();
+            }
+            if (connection != null) {
+                System.out.println("close connection");
+                connection.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("closeCrap exception");
+        }
+    }
+
+    public void sendMessage(Object object) {
+        try {
+            System.out.println("Message sent: " + object);
+            int counter = 0;
+            for (;;) {
+                if (out != null) {
+                    out.writeObject(object);
+                    out.flush();
+                    break;
+                }
+                else if (counter == 30) {
+                    System.out.println("Timeout while trying to send message. Output stream probably is not open.");
+                    break;
+                }
+                counter++;
+                T.SLEEP(100);
+            }
+        } catch (Exception ex) {
+            System.out.println("something went wrong with the host");
+            ex.printStackTrace();
+        }
+    }
+
+    private class myHandler extends Thread {
+
+        private Handler handler;
+
+        @Override
+        public void run() {
+            Looper.prepare();
+
+            handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    Object obj = msg.obj;
+                    System.out.println("Message came: " + obj);
+                    if (obj instanceof String) {
+                        String s = (String) obj;
+                        if (s.startsWith(T.SIGN_UP_CONFIRM)) {
+                            T.SIGN_UP_MESSAGE = s.substring(2);
+                        }
+                        else if (s.startsWith(T.PRIVATE_KEY)) {
+                            T.SIGN_UP_MESSAGE = s.substring(2);
+                        }
+                        else if (s.startsWith(T.LOG_IN_CONFIRM)) {
+                            T.LOG_IN_MESSAGE = s.substring(2);
+                        }
+                        else if (s.startsWith(T.OTP_CONFIRM)) {
+                            T.OTP_MESSAGE = s.substring(2);
+                        }
+                        else if (s.startsWith(T.MAIN_CONFIRM)) {
+                            T.MAIN_MESSAGE = s.substring(2);
+                        }
+                    }
+
+                }
+            };
+
+            Looper.loop();
+        }
+
+        public Handler getHandler() {
+            return this.handler;
+        }
+
+    }
+
+}
+
---
+++ app/java/com/sec/secureapp/general/T.java
@@ -0,0 +1,87 @@
+package com.sec.secureapp.general;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.sec.secureapp.database.DB;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class T {
+
+    public static DB DB;
+
+    public static String SERVER_IP = "zafeiratosv.ddns.net";
+    public static int SERVER_PORT = 54321;
+
+    public static String SUCCESS = "1";
+    public static String NOT_SUCCESS = "2";
+    public static String NAME_EXIST = "3";
+    public static String WRONG_CREDENTIALS = "4";
+    public static String WRONG_OTP = "5";
+    public static String OTP_ERROR = "6";
+    public static String PRIVATE_KEY_ACK = "7";
+
+    public static String SIGN_UP = " a";
+    public static String SIGN_UP_CONFIRM = " b";
+
+    public static String LOG_IN = " c";
+    public static String LOG_IN_CONFIRM = " d";
+
+    public static String OTP = " e";
+    public static String OTP_CONFIRM = " f";
+
+    public static String MAIN = " g";
+    public static String MAIN_CONFIRM = " h";
+
+    public static String PRIVATE_KEY = " i";
+
+    public static String SIGN_UP_MESSAGE = null;
+    public static String LOG_IN_MESSAGE = null;
+    public static String OTP_MESSAGE = null;
+    public static String MAIN_MESSAGE = null;
+
+    public static void VIEW_TOAST(final Context con, final String toast, final int dur) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            public void run() {
+                Toast.makeText(con, toast, dur).show();
+            }
+        });
+    }
+
+    public static void SLEEP(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getJson (String ... strings) {
+        JSONObject jo = new JSONObject();
+        HashMap <String, String> map = new HashMap<>();
+        int i = 0;
+        try {
+            for(;;) {
+                try {
+                    map.put(strings[i], strings[i + 1]);
+                    i += 2;
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    break;
+                }
+            }
+            for (String k : map.keySet()) {
+                jo.put(k, map.get(k));
+            }
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return jo;
+    }
+
+}
